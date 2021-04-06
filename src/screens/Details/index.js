b49be3a1bfee3ce66675083cf62aa1c56d3de023
Abby Hilman <abbyhilman@gmail.ccom>
2021-04-06 13:24:14 +0700

    add animation for detail

--- src/screens/Details/index.js
+++ src/screens/Details/index.js
@@ -1,38 +1,74 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 import {
   StyleSheet,
   ImageBackground,
   TouchableOpacity,
   SafeAreaView,
   View,
+  Animated,
   FlatList,
   Image,
   RefreshControl,
   Text,
+  Platform,
+  Dimensions,
 } from "react-native";
+import ProgressBar from "react-native-animated-progress";
 import { useDispatch, useSelector } from "react-redux";
 import { setLoading, getDetailPokemon } from "../../redux/action";
 import { Ionicons } from "@expo/vector-icons";
+import MaskedView from "@react-native-masked-view/masked-view";
+import Svg, { Rect } from "react-native-svg";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Details = ({ route, navigation }) => {
   const dispatch = useDispatch();
   const { pokemon } = route.params;
-  const [refreshing, setRefreshing] = useState(false);
   const { detailsPokemon } = useSelector((state) => state.homeReducer);
-  console.log(detailsPokemon.id);
+  const [refreshing, setRefreshing] = useState(false);
+  const [randomColor, setRandomColor] = useState(null);
+  const { width, height } = Dimensions.get("window");
+  const ITEM_SIZE = Platform.OS === "ios" ? width * 0.72 : width * 0.74;
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const BACKDROP_HEIGHT = height * 0.65;
+  const AnimatedSvg = Animated.createAnimatedComponent(Svg);
+
+  const getRandomColor = () => {
+    return (
+      "rgb(" +
+      Math.floor(Math.random() * 256) +
+      "," +
+      Math.floor(Math.random() * 256) +
+      "," +
+      Math.floor(Math.random() * 256) +
+      ")"
+    );
+  };
 
   useEffect(async () => {
-    dispatch(setLoading(true));
-    dispatch(getDetailPokemon(pokemon));
+    await dispatch(setLoading(true));
+    await dispatch(getDetailPokemon(pokemon));
+    setRandomColor(getRandomColor);
   }, [dispatch]);
 
-  let images = [
+  const images = [
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/${detailsPokemon.id}.png`,
     `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${detailsPokemon.id}.png`,
-    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${detailsPokemon.id}.png`,
     `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${detailsPokemon.id}.png`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${detailsPokemon.id}.png`,
     `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${detailsPokemon.id}.png`,
   ];
 
+  const imageBack = [
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/${detailsPokemon.id}.png`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/${detailsPokemon.id}.png`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${detailsPokemon.id}.png`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${detailsPokemon.id}.png`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${detailsPokemon.id}.png`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${detailsPokemon.id}.png`,
+  ];
+
+  useEffect(async () => {});
   const wait = (timeout) => {
     return new Promise((resolve) => {
       setTimeout(resolve, timeout);
@@ -46,12 +82,78 @@ const Details = ({ route, navigation }) => {
     wait(200).then(() => setRefreshing(false));
   }, [dispatch]);
 
+  const Backdrop = ({ image, scrollX }) => {
+    return (
+      <View style={{ height: BACKDROP_HEIGHT, width, position: "absolute" }}>
+        <FlatList
+          data={image}
+          keyExtractor={(item, index) => index}
+          renderItem={({ item, index }) => {
+            if (!item) {
+              return null;
+            }
+
+            const inputRange = [
+              (index - 2) * ITEM_SIZE,
+              (index - 1) * ITEM_SIZE,
+            ];
+
+            const translateX = scrollX.interpolate({
+              inputRange,
+              outputRange: [-width, 0],
+            });
+            return (
+              <MaskedView
+                style={{ position: "absolute" }}
+                maskElement={
+                  <AnimatedSvg
+                    width={width}
+                    height={height}
+                    viewBox={`0 0 ${width} ${height}`}
+                    style={{ transform: [{ translateX }] }}
+                  >
+                    <Rect
+                      x="0"
+                      y="0"
+                      width={width}
+                      height={height}
+                      fill="red"
+                    />
+                  </AnimatedSvg>
+                }
+              >
+                <Image
+                  source={{ uri: item }}
+                  key={index}
+                  style={{
+                    width,
+                    height: BACKDROP_HEIGHT,
+                    resizeMode: "cover",
+                  }}
+                />
+              </MaskedView>
+            );
+          }}
+        />
+        <LinearGradient
+          colors={["transparent", "white"]}
+          style={{
+            width,
+            height: BACKDROP_HEIGHT,
+            position: "absolute",
+            bottom: 0,
+          }}
+        />
+      </View>
+    );
+  };
   return (
     <SafeAreaView style={styles.page}>
+      <Backdrop image={imageBack} scrollX={scrollX} />
       <ImageBackground
-        source={require("../../assets/images/bgDetail.png")}
+        //source={require("../../assets/images/bgDetail.png")}
         style={styles.cover}
-        imageStyle={{ opacity: 0.3 }}
+        imageStyle={{ opacity: 0.1 }}
         resizeMode="stretch"
       >
         <View
@@ -91,7 +193,8 @@ const Details = ({ route, navigation }) => {
               style={{
                 flexDirection: "row",
                 justifyContent: "space-between",
-                marginHorizontal: 15,
+                paddingTop: "5%",
+                paddingLeft: 10,
               }}
             >
               <View
@@ -124,6 +227,7 @@ const Details = ({ route, navigation }) => {
                       width: 80,
                       height: 30,
                       borderRadius: 12,
+                      marginLeft: 10,
                     }}
                   >
                     <Text
@@ -143,22 +247,29 @@ const Details = ({ route, navigation }) => {
           </View>
           <Text
             style={{
-              alignSelf: "flex-end",
-              marginLeft: 90,
-              marginBottom: 30,
+              alignSelf: "center",
+              marginLeft: "80%",
               fontWeight: "bold",
               fontSize: 18,
+              position: "absolute",
             }}
           >
             # {detailsPokemon.base_experience}
           </Text>
         </View>
 
-        <FlatList
-          horizontal={true}
+        <Animated.FlatList
+          horizontal
           showsHorizontalScrollIndicator={false}
           data={images}
           keyExtractor={(item, index) => index}
+          snapToInterval={ITEM_SIZE}
+          bounces={false}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: true }
+          )}
+          scrollEventThrottle={16}
           refreshControl={
             <RefreshControl
               refreshing={refreshing}
@@ -166,27 +277,80 @@ const Details = ({ route, navigation }) => {
               enabled={true}
             />
           }
-          renderItem={({ item, index }) => (
-            <Image
-              source={{ uri: item }}
-              key={index}
-              style={{
-                width: 260,
-                height: 260,
-                shadowColor: "black",
-                shadowOffset: { width: 0, height: 7 },
-                shadowOpacity: 0.5,
-                shadowRadius: 10,
-                elevation: 14,
-                overflow: "hidden",
-                marginHorizontal: 40,
-              }}
-            />
-          )}
+          renderItem={({ item, index }) => {
+            const inputRange = [
+              (index - 2) * ITEM_SIZE,
+              index * ITEM_SIZE,
+              (index + 1) * ITEM_SIZE,
+            ];
+            const translateY = scrollX.interpolate({
+              inputRange,
+              outputRange: [100, -50, 100],
+            });
+            return (
+              <View style={{ width: ITEM_SIZE }}>
+                <Animated.View
+                  style={{
+                    marginHorizontal: 40,
+                    marginTop: 25,
+                    position: "absolute",
+                    transform: [{ translateY }],
+                  }}
+                >
+                  <Image
+                    source={{ uri: item }}
+                    key={index}
+                    style={{
+                      width: 260,
+                      height: 260,
+                      shadowColor: "black",
+                      shadowOffset: { width: 0, height: 7 },
+                      shadowOpacity: 0.5,
+                      shadowRadius: 10,
+                      overflow: "hidden",
+                    }}
+                  />
+                </Animated.View>
+              </View>
+            );
+          }}
         />
       </ImageBackground>
 
-      <View style={styles.content}></View>
+      <View style={styles.content}>
+        <Text style={{fontWeight: 'bold', fontSize: 24, marginBottom: 24}}>Basic Stat</Text>
+        <FlatList
+          data={detailsPokemon.stats}
+          keyExtractor={(item, index) => index}
+          renderItem={({ item, index }) => {
+            if (!item) {
+              return null;
+            }
+            return (
+              <>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: 'space-between',
+                    padding: 10,
+                    alignItems: "center",
+                  }}
+                >
+                  <Text style={{fontWeight: '500', fontSize: 14}}>{item.stat.name}</Text>
+                  <View style={{ width: 170, position: 'relative'}}>
+                    <ProgressBar
+                      progress={item.base_stat}
+                      height={7}
+                      backgroundColor={randomColor}
+                    />
+                  </View>
+                  <Text style={{position: 'absolute', alignSelf: 'center', marginHorizontal: '40%', fontWeight: 'bold'}}>{item.base_stat}</Text>
+                </View>
+              </>
+            );
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
